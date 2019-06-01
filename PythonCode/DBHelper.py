d3370da044a693c5284ea525ba67493eb86aea92
JeongYeongHun <jyh1519@naver.com>
2019-06-01 22:39:14 +0900

    SQLite3를 이용한 db생성, db관련 모듈 생성, 키 상태 인식 개선

---
+++ PythonCode/DBHelper.py
@@ -0,0 +1,36 @@
+import pygame
+import sqlite3
+
+def insert(name, w, a, s, d, shift, c, alt, tab, space, one, two, three, esc, mousemove, e, q):
+    conn = sqlite3.connect("key.db")
+    cur = conn.cursor()
+    sql = "insert into key(name, w, a, s, d, shift, c, alt, tab, space, one, two, three, esc, lclick, rclick, mousemove, e, q) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    cur.execute(sql, (name, w, a, s, d, shift, c, alt, tab, space, one, two, three, esc, 'lclick', 'rclick', mousemove, e, q))
+    conn.commit()
+    conn.close()
+
+def load(num):
+    conn = sqlite3.connect("key.db")
+    cur = conn.cursor()
+    sql = "select * from key where id=?"
+    cur.execute(sql, (num,))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def list():
+    conn = sqlite3.connect("key.db")
+    cur = conn.cursor()
+    cur.execute("select id,name from key")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+if __name__ == "__main__":
+    print("test code")
+    
+    rows = list()
+    print ("list : ", rows, "\n")
+
+    row = load(1)
+    print ("load : ", row, "\n")
