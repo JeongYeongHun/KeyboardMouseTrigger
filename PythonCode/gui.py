a0126ca5bf603face3f64e877adaca978d1492f9
liacrack94 <liacrack94@naver.com>
2019-06-09 20:49:04 +0900

    GUI와 사용자 저장기능 연결, 마우스 감도 조절 완성

--- PythonCode/gui.py
+++ PythonCode/gui.py
@@ -42,15 +42,37 @@ def start(select):
 def keyInput(event):
     print("s")
 
+def settingSave(value):
+    setlist = []
+    setlist.append(txt1.get())
+    setlist.append(txt2.get())
+    setlist.append(txt3.get())
+    setlist.append(txt4.get())
+    setlist.append(txt5.get())
+    setlist.append(txt6.get())
+    setlist.append(txt7.get())
+    setlist.append(txt8.get())
+    setlist.append(txt9.get())
+    setlist.append(txt10.get())
+    setlist.append(txt11.get())
+    setlist.append(txt12.get())
+    setlist.append(txt13.get())
+    setlist.append('esc')
+    setlist.append(value/50)
+    setlist.append(txt15.get())
+    setlist.append(txt16.get())
+
+    DBHelper.insert(setlist[0],setlist[1],setlist[2],setlist[3],setlist[4],setlist[5],setlist[6],setlist[7],setlist[8],setlist[9],setlist[10],setlist[11],setlist[12],setlist[13],setlist[14],setlist[15],setlist[16])
+    
+    processoExit()
 
 def processOption():        #설정버튼 클릭 이벤트처리
-    global windowo
+    global windowo, txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9, txt10, txt11, txt12, txt13, txt14, txt15, txt16
     windowo = Tk()
     windowo.title("Setting")
     windowo.geometry("520x480+0+0")
 
     lbl0 = Label(windowo, width=7, text='키설정')
-
     lbl1 = Label(windowo, width=12, text='사용자이름')
     lbl2 = Label(windowo, width=12, text='앞으로가기')
     lbl3 = Label(windowo, width=12, text='왼쪽으로가기')
@@ -64,11 +86,8 @@ def processOption():        #설정버튼 클릭 이벤트처리
     lbl11 = Label(windowo, width=12, text='1번무기로교체')
     lbl12 = Label(windowo, width=12, text='2번무기로교체')
     lbl13 = Label(windowo, width=12, text='3번무기로교체')
-    lbl14 = Label(windowo, width=12, text='감도')
     lbl15 = Label(windowo, width=12, text='오른쪽기울이기')
     lbl16 = Label(windowo, width=12, text='왼쪽기울이기')
-
-    rows = DBHelper.load(1) #normal
 
     txt1 = Entry(windowo, width=7)
     txt2 = Entry(windowo, width=7)
@@ -83,9 +102,11 @@ def processOption():        #설정버튼 클릭 이벤트처리
     txt11 = Entry(windowo,  width=7)
     txt12 = Entry(windowo, width=7)
     txt13 = Entry(windowo, width=7)
-    txt14 = Entry(windowo, width=7)
     txt15 = Entry(windowo,  width=7)
     txt16 = Entry(windowo,  width=7)
+
+    rows = DBHelper.load(1) #normal
+
     txt2.insert(0,rows[2])
     txt3.insert(0,rows[3])
     txt4.insert(0,rows[4])
@@ -98,9 +119,9 @@ def processOption():        #설정버튼 클릭 이벤트처리
     txt11.insert(0,rows[11])
     txt12.insert(0,rows[12])
     txt13.insert(0,rows[13])
-    txt14.insert(0,rows[14])
     txt15.insert(0,rows[18])
     txt16.insert(0,rows[19])
+
     txt2.configure(state='readonly')
     txt3.configure(state='readonly')
     txt4.configure(state='readonly')
@@ -113,7 +134,6 @@ def processOption():        #설정버튼 클릭 이벤트처리
     txt11.configure(state='readonly')
     txt12.configure(state='readonly')
     txt13.configure(state='readonly')
-    txt14.configure(state='readonly')
     txt15.configure(state='readonly')
     txt16.configure(state='readonly')
     txt2.bind("<Button-1>", keyInput)
@@ -128,15 +148,13 @@ def processOption():        #설정버튼 클릭 이벤트처리
     txt11.bind("<Button-1>", keyInput)
     txt12.bind("<Button-1>", keyInput)
     txt13.bind("<Button-1>", keyInput)
-    txt14.bind("<Button-1>", keyInput)
     txt15.bind("<Button-1>", keyInput)
     txt16.bind("<Button-1>", keyInput)
-
 
     lbl17 = Label(windowo, width=13, text='마우스감도조절')
     bar1 = Scale(windowo, orient='horizontal', length=450)
     bar1.set(50)
-    btn3 = Button(windowo, width=13, text='저장', command=lambda: callback(bar1.get()))
+    btn3 = Button(windowo, width=13, text='저장', command=lambda: settingSave(bar1.get()))
     btn4 = Button(windowo, width=13, text='취소', command=processoExit)
 
     lbl0.place(x=10, y=10)
@@ -167,12 +185,10 @@ def processOption():        #설정버튼 클릭 이벤트처리
     txt12.place(x=360, y=143)
     lbl13.place(x=270, y=170)
     txt13.place(x=360, y=173)
-    lbl14.place(x=270, y=200)
-    txt14.place(x=360, y=203)
-    lbl15.place(x=270, y=230)
-    txt15.place(x=360, y=233)
-    lbl16.place(x=270, y=260)
-    txt16.place(x=360, y=263)
+    lbl15.place(x=270, y=200)
+    txt15.place(x=360, y=203)
+    lbl16.place(x=270, y=230)
+    txt16.place(x=360, y=233)
 
     lbl17.place(x=10, y=315)
     bar1.place(x=30, y=335)
@@ -195,6 +211,24 @@ windowo = None
 window = Tk()
 window.title("K/M Trigger")
 window.geometry("300x250+0+0")
+
+
+txt1 = Entry(windowo, width=7)
+txt2 = Entry(windowo, width=7)
+txt3 = Entry(windowo, width=7)
+txt4 = Entry(windowo, width=7)
+txt5 = Entry(windowo, width=7)
+txt6 = Entry(windowo, width=7)
+txt7 = Entry(windowo, width=7)
+txt8 = Entry(windowo, width=7)
+txt9 = Entry(windowo, width=7)
+txt10 = Entry(windowo, width=7)
+txt11 = Entry(windowo, width=7)
+txt12 = Entry(windowo, width=7)
+txt13 = Entry(windowo, width=7)
+txt15 = Entry(windowo, width=7)
+txt16 = Entry(windowo, width=7)
+
 
 btn1 = Button(window, width=13, text='게임시작' ,command=processStart)
 btn2 = Button(window, width=13, text='설정', command=processOption)
